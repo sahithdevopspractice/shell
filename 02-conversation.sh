b8e942141760cfccd692b370ab34ba25ef5836cb
sahithdevopspractice <sahithdevopspractice@gmail.com>
2025-05-21 11:56:37 -0400

    Conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Trump: Hey Elon How are you?"
+echo "Elon:: Am good how are you"
+echo "Trump: Am fine too, whatsup?"
+echo "Elon:: Just going to Mars, would you like to join?"
+echo "Trump: Sorry, you cary on, will join with once you are back"
